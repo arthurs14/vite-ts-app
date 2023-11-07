cba6abcae66b394371042b822c7f251f0271c600
arthurs14 <arthurcs426@gmail.com>
2023-11-06 16:28:10 -0800

    added new route unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,28 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
+import { MemoryRouter } from 'react-router-dom';
 
-import App from './App';
+import { WrappedApp, App } from './App';
 
 describe('App', () => {
   it('Renders Hello World', () => {
     // Arrange
-    render(<App />);
+    render(<WrappedApp />);
     // Act
     // Expect
-    const test = screen.getByRole('heading', { level: 1 }).innerHTML;
-    expect(test).toBe('Hello World!');
+    const helloWorld = screen.getByRole('heading', { level: 1 }).innerHTML;
+    expect(helloWorld).toBe('Hello World!');
+  });
+
+  it('Renders not found in invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/testing-this-path']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    const notFound = screen.getByRole('heading', { level: 1 }).innerHTML;
+    expect(notFound).toBe('Not Found');
   });
 });
